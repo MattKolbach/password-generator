e15b07671165126b6cc80e1b12892e02a54f6415
Matt Kolbach <mattkolbach@yahoo.com>
2021-12-19 14:19:59 -0600

    added if's, and for loop, project complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,22 @@
 // Assignment code here
 
-
 var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 var numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-var specialCharacter = ["!", "#", "$", "%", "&", "'", "(",")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "{", "}", "|", "~"]
+var specialCharacterArray = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "{", "}", "|", "~"]
+
+
+
 
 
 
 //main function, runs when button is pressed
 function generatePassword() {
+  var passwordArray = []
+  var combinedArrays = []
   //prompt for password criteria
   //prompt for length 8-128 characters
   var passwordlength = window.prompt("Pick a password length from 8 to 128 characters in length.");
@@ -23,23 +27,44 @@ function generatePassword() {
   var passwordCharacterTypes = passwordCharacters();
   //console.table(passwordCharacterTypes);
 
-  //make an array to capture variables picked
+
+  if (passwordCharacterTypes.lowerCase) {
+    combinedArrays = combinedArrays.concat(lowerCaseArray)
+  };
+
+  if (passwordCharacterTypes.upperCase) {
+    combinedArrays = combinedArrays.concat(upperCaseArray)
+  };
+
+  if (passwordCharacterTypes.numeric) {
+    combinedArrays = combinedArrays.concat(numericArray)
+  };
+
+  if (passwordCharacterTypes.specialCharacter) {
+    combinedArrays = combinedArrays.concat(specialCharacterArray)
+  };
+
+  for (var i = 0; i < passwordlength; i++) {
+    var randomIndexNumber = getRandomIntInclusive(0, combinedArrays.length - 1);
+    var randomCharacter = combinedArrays[randomIndexNumber]
+    passwordArray.push(randomCharacter)
+  }
+  return passwordArray.join("");
+  //make an array to capture variables picked -DONE- var passwordArray = []
   //insert a for loop here
   //run for passwordlength number of times
   //grab variables from however many trues are returned on passwordCharacters, randomly
   //array.push to add to end of an array
-  //after for loop ends, convert array into a string - arrayname.join()  see line 38
+  //after for loop ends, convert array into a string - arrayname.join()  see consol.log(lowerCaseArray.join("")) below
   //array.join will turn above array characters into a string, empty quotes for no spaces between integers
   //use return as last thing to return result of array.join to writepassword function as the string 'password'
 }
 
-var testIndex = getRandomIntInclusive(0, lowerCaseArray.length-1);
-var test = lowerCaseArray[testIndex]
-console.log(test, testIndex);
+// var testIndex = getRandomIntInclusive(0, lowerCaseArray.length-1);
+// var test = lowerCaseArray[testIndex]
+// console.log(test, testIndex);
 
-console.log(lowerCaseArray.join(""))
-
-
+// console.log(lowerCaseArray.join(""))
 
 
 function passwordCharacters() {
@@ -67,9 +92,6 @@ function passwordCharacters() {
 
 
 
-
-
-//I dont know if this is helpful
 //random integer between 2 values, inclusive
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
@@ -79,18 +101,18 @@ function getRandomIntInclusive(min, max) {
 }
 
 
-  //generate a password using selected criteria
+//generate a password using selected criteria
 
 
 
-  //display as an alert or written to page<<<write it to the page
-  
+//display as an alert or written to page<<<write it to the page
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword() {  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
