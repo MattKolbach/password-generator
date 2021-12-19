6c912356feb8f4ec630590b1faa08f2a22f876ec
Matt Kolbach <mattkolbach@yahoo.com>
2021-12-18 22:54:08 -0600

    wrote psudocode for how to solve

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
 // Assignment code here
 
 
+var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+var numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+var specialCharacter = ["!", "#", "$", "%", "&", "'", "(",")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "{", "}", "|", "~"]
+
+
 
 //main function, runs when button is pressed
 function generatePassword() {
@@ -12,8 +21,27 @@ function generatePassword() {
     return generatePassword();
   }
   var passwordCharacterTypes = passwordCharacters();
-  console.table(passwordCharacterTypes);
+  //console.table(passwordCharacterTypes);
+
+  //make an array to capture variables picked
+  //insert a for loop here
+  //run for passwordlength number of times
+  //grab variables from however many trues are returned on passwordCharacters, randomly
+  //array.push to add to end of an array
+  //after for loop ends, convert array into a string - arrayname.join()  see line 38
+  //array.join will turn above array characters into a string, empty quotes for no spaces between integers
+  //use return as last thing to return result of array.join to writepassword function as the string 'password'
 }
+
+var testIndex = getRandomIntInclusive(0, lowerCaseArray.length-1);
+var test = lowerCaseArray[testIndex]
+console.log(test, testIndex);
+
+console.log(lowerCaseArray.join(""))
+
+
+
+
 function passwordCharacters() {
   //character types to use, yes/ no || true/ false
   //lower case
@@ -37,10 +65,26 @@ function passwordCharacters() {
   }
 }
 
+
+
+
+
+//I dont know if this is helpful
+//random integer between 2 values, inclusive
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+  //the maximum is inclusive and the minimum is inclusive
+}
+
+
   //generate a password using selected criteria
-  //display as an alert or written to page
 
 
+
+  //display as an alert or written to page<<<write it to the page
+  
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
